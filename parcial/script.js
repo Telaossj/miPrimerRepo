8da46be08815fd56442cd7e8cc7ef617dbcf8c74
GastonLuciano <gastontelao11@gmail.com>
2024-05-27 21:20:10 -0300

    ACTUALICE LA PAGINA

--- parcial/script.js
+++ parcial/script.js
@@ -2,11 +2,12 @@ const header = document.querySelector("header");
 const links = document.querySelectorAll("a");
 
 header.addEventListener("click", () => {
-    header.style.color = "green";
+    header.style.backgroundColor = "#0056b3";
 });
-links.forEach(link =>{
-    link.addEventListener("click", (event) =>{
+
+links.forEach(link => {
+    link.addEventListener("click", (event) => {
         event.preventDefault();
-        alert("Acabas de hacer clic en el enlace: ${link.href}");
+        alert(`Acabas de hacer clic en el enlace: ${link.href}`);
     });
 });
